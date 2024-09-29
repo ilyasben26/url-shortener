5f8cf35e329faf4b7e34e4ccd623845d0f37accf
IlyasBen <73348981+ilyasben26@users.noreply.github.com>
2024-09-29 08:35:13 +0200

    added QR code to analytics dashboard

--- src/app/analytics/[shortcode]/_components/analytics.tsx
+++ src/app/analytics/[shortcode]/_components/analytics.tsx
@@ -2,6 +2,7 @@ import { getAnalytics } from "~/server/queries";
 import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 import CountryChart from "./country-chart";
 import DeviceChart from "./device-chart";
+import { QrCode } from "./qr-code";
 
 interface AnalyticsProps {
     shortcode: string;
@@ -9,10 +10,22 @@ interface AnalyticsProps {
 
 export default async function Analytics({ shortcode }: AnalyticsProps) {
     const analytics = await getAnalytics(shortcode);
+    const shortened_url = "https://" + process.env.NEXT_PUBLIC_URL + '/' + shortcode;
 
     return (
         <div className="container mx-auto p-4">
             <h1 className="text-2xl font-bold mb-4">Analytics Dashboard</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-6">
+                <Card>
+                    <CardHeader>
+                        <CardTitle> QR Code</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <QrCode shortened_url={shortened_url} />
+                    </CardContent>
+                </Card>
+            </div>
+
 
             {/* KPI Cards */}
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-6">
@@ -35,10 +48,13 @@ export default async function Analytics({ shortcode }: AnalyticsProps) {
                         </p>
                     </CardContent>
                 </Card>
+
             </div>
+
 
             {/* Charts */}
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+
                 {/* Country Visits Chart */}
                 <Card>
                     <CardHeader>
@@ -62,6 +78,8 @@ export default async function Analytics({ shortcode }: AnalyticsProps) {
                         </div>
                     </CardContent>
                 </Card>
+                {/* TODO: Delete Shortened URL  */}
+
             </div>
         </div>
     );
---
+++ src/app/analytics/[shortcode]/_components/qr-code.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import QRCode from "qrcode";
+import { useEffect, useState } from "react";
+import Image from 'next/image'
+import { Button } from "~/components/ui/button";
+
+interface QrCodeProps {
+    shortened_url: string
+}
+
+export function QrCode({ shortened_url }: QrCodeProps) {
+    const [src, setSrc] = useState<string>('');
+
+    const generate = () => {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+        QRCode.toDataURL(shortened_url)
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+            .then(setSrc)
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+            .catch(err => console.error(err));
+    };
+
+    const handleDownload = () => {
+        if (src) {
+            const link = document.createElement('a');
+            link.href = src;
+            link.download = 'qrcode.png';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
+    };
+
+    // Generate the QR code on component mount or when the shortened_url changes
+    useEffect(() => {
+        generate();
+    }, [generate, shortened_url]);
+
+    return (
+        <div className="flex flex-col items-center">
+            <Image src={src} alt="QR code" width={250} height={250} />
+
+            <Button className="mb-3 w-48" onClick={generate}>Regenerate</Button>
+            <Button className="w-48" onClick={handleDownload}>Download</Button>
+
+        </div>
+    );
+}
